0b15b03092c15cbfe18d69ffe32a213213604d7d
wanglifa <15806553882@163.com>
2018-08-20 00:21:13 +0800

    添加 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,8 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+            @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -11,12 +13,16 @@
         //props: ['icon','iconPosition']
         props: {
             icon: {},
+            loading: {
+                type: Boolean,
+                default: false
+            },
             iconPosition: {
                 type: String,
                 //默认值是left
                 default: 'left',
                 //validator函数用于拿到你传入的参数
-                validator(value){
+                validator(value) {
                     return !(value !== 'left' && value !== 'right');
                 }
             }
@@ -24,7 +30,16 @@
     }
 </script>
 <style lang="scss">
-    .g-button{
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
@@ -36,31 +51,34 @@
         justify-content: center;
         align-items: center;
         padding: 0 1em;
-        &:hover{
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
+        &:active {
             background-color: var(--button-active-bg);
         }
-        &:focus{
+        &:focus {
             outline: none;
         }
-        > .icon{
+        > .icon {
             order: 1;
             margin-right: .1em;
         }
-        > .content{
+        > .content {
             order: 2;
         }
-        &.icon-right{
-            > .icon{
+        &.icon-right {
+            > .icon {
                 order: 2;
                 margin-right: 0;
                 margin-left: .1em;
             }
-            > .content{
+            > .content {
                 order: 1;
             }
+        }
+        .loading {
+            animation: spin 2s infinite linear;
         }
     }
 
