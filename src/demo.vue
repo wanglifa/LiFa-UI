37b89269b14b6a8be9a83c28933fccbd222e40a3
wanglifa <wanglifa1995@gmail.com>
2019-02-06 23:16:24 +0800

    改用vue-cli构建项目

---
+++ src/demo.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <g-cascader :source="source" height="200px"></g-cascader>
+    </div>
+</template>
+
+<script>
+    import Button from './button/button.vue'
+    import Cascader from  './cascader.vue'
+    export default {
+        name: "demo",
+        components: {
+            'g-button': Button,
+            'g-cascader': Cascader
+        },
+        data(){
+            return {
+                source: [
+                    {
+                        name: '浙江',
+                        children: [
+                            {
+                                name: '杭州',
+                                children: [
+                                    {name: '上城'},
+                                    {name: '下城'},
+                                    {name: '江干'}
+                                ]
+                            },{
+                                name: '嘉兴',
+                                children: [
+                                    {name: '南湖'},
+                                    {name: '秀洲'},
+                                    {name: '嘉善'}
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: '福建',
+                        children: [
+                            {
+                                name: '福州',
+                                children: [
+                                    {name: '鼓楼'},
+                                    {name: '台江'},
+                                    {name: '苍山'}
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    *{
+        margin: 0;
+        padding: 0;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    body{
+        font-size: var(--font-size);
+    }
+
+    html{
+        --font-size: 14px;
+    }
+
+</style>
