1a0a5e7b03d2ba87554a80608ab7be49e5f4e499
wanglifa <wanglifa1995@gmail.com>
2019-02-22 20:16:45 +0800

    修改了手机滑动动画错误的bug

--- src/slides.vue
+++ src/slides.vue
@@ -7,11 +7,11 @@
             <transition name="fade">
                 <div class="icon-wrapper">
                     <span class="left" @click="onClickPrev">
-                    <i>&lt;</i>
-                </span>
+                        <i>&lt;</i>
+                    </span>
                     <span class="right" @click="onClickNext">
-                    <i>&gt;</i>
-                </span>
+                        <i>&gt;</i>
+                    </span>
                 </div>
             </transition>
             <div class="lf-slides-wrapper">
@@ -37,23 +37,23 @@
             },
             autoPlay: {
                 type: Boolean,
-                default: false
+                default: true
             }
         },
-        data(){
-          return {
-              childrenLength: 0,
-              lastSelectedIndex: undefined,
-              timerId: null,
-              touchStart: null,
-              isClickArrow: false,
-              arrowClickInfo: false
-          }
+        data() {
+            return {
+                childrenLength: 0,
+                lastSelectedIndex: undefined,
+                timerId: null,
+                touchStart: null,
+                isClickArrow: false,
+                arrowClickInfo: false
+            }
         },
         mounted() {
             this.childrenLength = this.$children.length
             this.updateChildren()
-            if(this.autoPlay){
+            if (this.autoPlay) {
                 this.automaticPlay()
             }
         },
@@ -61,111 +61,117 @@
             this.updateChildren()
         },
         computed: {
-          selectedIndex(){
-              return this.names.indexOf(this.selected) || 0
-          },
-          names(){
-              return this.$children.map(vm=>vm.name)
-          }
+            selectedIndex() {
+                return this.names.indexOf(this.selected) || 0
+            },
+            names() {
+                return this.$children.map(vm => vm.name)
+            }
         },
         methods: {
-            onClickPrev(){
+            onClickPrev() {
                 this.isClickArrow = true
-              this.select(this.selectedIndex - 1)
+                this.select(this.selectedIndex - 1)
                 this.isClickArrow = false
             },
-            onClickNext(){
+            onClickNext() {
                 this.isClickArrow = true
-              this.select(this.selectedIndex + 1)
+                this.select(this.selectedIndex + 1)
                 this.isClickArrow = false
             },
-            onTouchStart(e){
-                if(e.touches.length > 1){return}
-                this.touchStart = {clientX:e.touches[0].clientX,clientY:e.touches[0].clientY}
+            onTouchStart(e) {
+                if (e.touches.length > 1) {
+                    return
+                }
+                this.touchStart = {clientX: e.touches[0].clientX, clientY: e.touches[0].clientY}
                 this.pause()
             },
-            onTouchMove(e){
+            onTouchMove(e) {
                 console.log(e)
             },
-            onTouchEnd(e){
-                let {clientX,clientY} = e.changedTouches[0]
-                let [x1,y1] = [this.touchStart.clientX,this.touchStart.clientY]
-                let [x2,y2] = [clientX,clientY]
-                let distance = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2))
-                let deltaY = Math.abs(y2-y1)
+            onTouchEnd(e) {
+                let {clientX, clientY} = e.changedTouches[0]
+                let [x1, y1] = [this.touchStart.clientX, this.touchStart.clientY]
+                let [x2, y2] = [clientX, clientY]
+                let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+                let deltaY = Math.abs(y2 - y1)
                 let rate = distance / deltaY
-                if(rate > 2){
-                    if(clientX > this.touchStart && this.touchStart.clientX){
+                if (rate > 2) {
+                    if (clientX > this.touchStart && this.touchStart.clientX) {
+                        this.isClickArrow = true
                         this.select(this.selectedIndex - 1)
-                    }else{
+                        this.isClickArrow = false
+                    } else {
+                        this.isClickArrow = true
                         this.select(this.selectedIndex + 1)
+                        this.isClickArrow = false
                     }
                 }
-                if(this.autoPlay){
+                if (this.autoPlay) {
                     this.automaticPlay()
                 }
             },
-            onMouseEnter(){
+            onMouseEnter() {
                 this.pause()
             },
-            onMouseLeave(){
-              if(this.autoPlay){
-                  this.automaticPlay()
-              }
+            onMouseLeave() {
+                if (this.autoPlay) {
+                    this.automaticPlay()
+                }
             },
-            pause(){
-              window.clearTimeout(this.timerId)
-              this.timerId = null
+            pause() {
+                window.clearTimeout(this.timerId)
+                this.timerId = null
             },
-            updateChildren(){
+            updateChildren() {
                 let selected = this.getSelected()
-                this.$children.forEach((vm)=>{
+                this.$children.forEach((vm) => {
                     let reverse = this.selectedIndex > this.lastSelectedIndex ? false : true
                     //如果是自动滚动的情况下
-                    if(this.timerId || this.arrowClickInfo){
+                    if (this.timerId || this.arrowClickInfo) {
                         //如果上一张是第一张，当前这张是最后一张（也就是反向动画的时候）就让它依然是反向动画
-                        if(this.lastSelectedIndex === 0 && this.selectedIndex === this.names.length-1){
+                        if (this.lastSelectedIndex === 0 && this.selectedIndex === this.names.length - 1) {
                             reverse = true
                         }
                         //如果上一张是最后一张，当前这张是第一张（也就是正向动画的时候）就让它依然是正向
-                        if(this.lastSelectedIndex === this.names.length-1 && this.selectedIndex === 0){
+                        if (this.lastSelectedIndex === this.names.length - 1 && this.selectedIndex === 0) {
                             reverse = false
                         }
                     }
                     vm.reverse = reverse
-                    this.$nextTick(()=>{
+                    this.$nextTick(() => {
                         vm.selected = selected
                     })
                 })
             },
-            automaticPlay(){
+            automaticPlay() {
                 //如果当前正在轮播中就不再次执行这个方法
-                if(this.timerId){
+                if (this.timerId) {
                     return
                 }
                 let selected = this.getSelected()
                 //拿到初始的索引值
                 let index = this.names.indexOf(selected)
-                let run = ()=>{
-                    this.newIndex = index +1
+                let run = () => {
+                    this.newIndex = index + 1
                     this.select(this.newIndex)
                     index = this.newIndex
-                    this.timerId =setTimeout(()=>{
+                    this.timerId = setTimeout(() => {
                         run()
-                    },3000)
+                    }, 3000)
                 }
                 this.timerId = setTimeout(run, 3000)
             },
-            getSelected(){
+            getSelected() {
                 let first = this.$children[0]
                 return this.selected || first.$attrs.name
             },
-            select(newIndex){
+            select(newIndex) {
                 this.arrowClickInfo = this.isClickArrow
-                if(newIndex < 0){
+                if (newIndex < 0) {
                     newIndex = this.names.length - 1
                 }
-                if(newIndex >= this.names.length){
+                if (newIndex >= this.names.length) {
                     newIndex = 0
                 }
                 //让newIndex等于条件内的newIndex
@@ -173,7 +179,7 @@
                 //当选中新的index的时候，就把旧的index赋给lastSelectedIndex
                 this.lastSelectedIndex = this.selectedIndex
                 //然后把新的index和选中值传给selected
-                this.$emit('update:selected',this.names[newIndex])
+                this.$emit('update:selected', this.names[newIndex])
             }
         }
     }
@@ -186,6 +192,7 @@
         overflow: hidden;
         position: relative;
         .left,.right{
+            cursor: pointer;
             position: absolute;
             top: 50%;
             transform: translateY(-50%);
