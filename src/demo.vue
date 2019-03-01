d68455f1a796410b5eed4404f0d2d073c52f271d
wanglifa <wanglifa1995@gmail.com>
2019-03-01 15:39:05 +0800

    nav基本结构确定

--- src/demo.vue
+++ src/demo.vue
@@ -1,35 +1,28 @@
 <template>
     <div>
-        <lf-slides :selected.sync="selected" trigger="hover">
-            <lf-slides-item name="1">
-                <div class="box">1</div>
-            </lf-slides-item>
-            <lf-slides-item name="2">
-                <div class="box">2</div>
-            </lf-slides-item>
-            <lf-slides-item name="3">
-                <div class="box">3</div>
-            </lf-slides-item>
-            <lf-slides-item name="4">
-                <div class="box">4</div>
-            </lf-slides-item>
-        </lf-slides>
+        <lf-nav :selected.sync="selected">
+            <lf-nav-item name="home">首页</lf-nav-item>
+            <lf-nav-item name="about">关于</lf-nav-item>
+            <lf-nav-item name="hire">招聘</lf-nav-item>
+        </lf-nav>
     </div>
 </template>
 
 <script>
-    import LfSlides from './slides/slides.vue'
-    import LfSlidesItem from './slides/slides-item.vue'
 
+    import LfNav from './nav/nav.vue'
+    import LfNavItem from './nav/nav-item.vue'
+    import LfSubNav from './nav/sub-nav.vue'
     export default {
         name: "demo",
         components: {
-            LfSlides,
-            LfSlidesItem
+            LfNav,
+            LfNavItem,
+            LfSubNav
         },
         data() {
             return {
-                selected: '1'
+                selected: ['home']
             }
         },
         methods: {},
@@ -40,13 +33,6 @@
 </script>
 
 <style scoped>
-    .box {
-        width: 100%;
-        height: 400px;
-        background: gray;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+
 
 </style>
