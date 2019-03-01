13b54447ef1a6323d0844645df17bb42de27702b
wanglifa <wanglifa1995@gmail.com>
2019-03-01 22:03:23 +0800

    支持任意层级的nav

--- src/demo.vue
+++ src/demo.vue
@@ -2,7 +2,22 @@
     <div>
         <lf-nav :selected.sync="selected">
             <lf-nav-item name="home">首页</lf-nav-item>
-            <lf-nav-item name="about">关于</lf-nav-item>
+            <lf-sub-nav>
+                <template slot="title">关于</template>
+                <lf-nav-item name="culture">企业文化</lf-nav-item>
+                <lf-nav-item name="developers">开发团队</lf-nav-item>
+                <lf-sub-nav>
+                    <template slot="title">联系方式</template>
+                    <lf-nav-item name="wechat">微信</lf-nav-item>
+                    <lf-nav-item name="qq">QQ</lf-nav-item>
+                    <lf-sub-nav>
+                        <template slot="title">手机</template>
+                        <lf-nav-item name="cm">移动</lf-nav-item>
+                        <lf-nav-item name="cu">联通</lf-nav-item>
+                        <lf-nav-item name="cn">电信</lf-nav-item>
+                    </lf-sub-nav>
+                </lf-sub-nav>
+            </lf-sub-nav>
             <lf-nav-item name="hire">招聘</lf-nav-item>
         </lf-nav>
     </div>
@@ -22,7 +37,7 @@
         },
         data() {
             return {
-                selected: ['home']
+                selected: ['culture']
             }
         },
         methods: {},
