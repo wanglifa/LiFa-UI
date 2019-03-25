22b7bf34b08d38cf2053a59c07f0d5dc8e927557
wanglifa <wanglifa1995@gmail.com>
2019-03-25 17:39:57 +0800

    upload基本功能实现

--- src/demo.vue
+++ src/demo.vue
@@ -1,117 +1,40 @@
 <template>
     <div>
-        {{selectedItem}}
-        <div style="margin: 28px">
-            <lf-table :columns="columns" :data-source="dataSource"
-                      bordered :selected-item.sync="selectedItem" :order-by.sync="orderBy"
-                      @update:orderBy="changeOrder" :loading="loading" :height="400"
-                      expend-field="description" checkable
-            >
-                <template slot-scope="xxx">
-                    <lf-button @click="edit(xxx.item.id)">编辑</lf-button>
-                    <lf-button @click="view(xxx.item.id)">查看</lf-button>
-                </template>
-            </lf-table>
-        </div>
-        <div style="margin: 28px">
-            <lf-table :columns="columns" :data-source="dataSource" compact :striped="false"></lf-table>
-        </div>
-        <div style="margin: 28px">
-            <lf-pager :total-page="20" :current-page.sync="currentPage"></lf-pager>
-        </div>
+        <lf-upload accept="image/*" action="http://127.0.0.1:3000/upload" name="file"
+            :file-list.sync="fileList" :parse-response="parseResponse" @update:fileList="x"
+        >
+            <button>上传</button>
+            <div slot="tips">
+                <div>只能上传300kb以内的png、jpeg文件</div>
+            </div>
+        </lf-upload>
     </div>
 </template>
 
 <script>
-    import LfPager from './pager.vue'
-    import LfTable from './table.vue'
-    import LfButton from './button/button.vue'
+    import LfUpload from './upload.vue'
     export default {
         name: "demo",
         components: {
-            LfPager,
-            LfTable,
-            LfButton
+            LfUpload
         },
         data() {
             return {
-                currentPage: 2,
-                selected: [],
-                selectedItem: [],
-                columns: [
-                    //表头每一列显示的文本和字段
-                    {text: '姓名', field: 'name', width: 100},
-                    {text: '分数', field: 'score',width: 100},
-                    {text: '年龄', field: 'age'}
-                ],
-                orderBy: {//true:开启排序，但是不确定asc desc
-                    name: true,
-                    score: 'desc'
-                },
-                dataSource: [
-                    {id: 1, name: '发发', score: 100, age:18, description: '你最帅'},
-                    {id: 2, name: '琳琳', score: 99, age: 16, description: '为啥不做我媳妇'},
-                    {id: 3, name: '西西', score: 99, age: 20, description: '好累啊'},
-                    {id: 4, name: '泳儿', score: 99, age: 21},
-                    {id: 5, name: '美美', score: 99, age: 22},
-                    {id: 6, name: '阿宇', score: 99, age: 26},
-                    {id: 7, name: '发发', score: 100, age:18},
-                    {id: 8, name: '琳琳', score: 99, age: 16},
-                    {id: 9, name: '西西', score: 99, age: 20},
-                    {id: 10, name: '泳儿', score: 99, age: 21},
-                    {id: 11, name: '美美', score: 99, age:18},
-                    {id: 12, name: '阿宇', score: 99, age: 16}
-                ],
-                key: '',
-                loading: false
+                fileList: []
             }
         },
         methods: {
-            changeOrder(data){
-                this.loading = true
-                this.$nextTick(()=>{
-                    setTimeout(()=>{
-                        let type
-                        let arr = this.dataSource.map(item=>{
-                            type = typeof item[this.key]
-                            return item[this.key]
-                        })
-                        if( data[this.key]=== 'asc'){
-                            if(type === 'number'){
-                                arr.sort((a,b)=>a-b)
-                            }else{
-                                arr.sort((a, b) => b.localeCompare(a, 'zh-Hans-CN', {sensitivity: 'accent'}))
-                            }
-                        }else if(data[this.key] === 'desc'){
-                            if(type === 'number'){
-                                arr.sort((a,b)=>b-a)
-                            }else{
-                                arr.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'}))
-                            }
-                        }
-                        arr.map((item,index)=>{
-                            this.dataSource[index][this.key]=item
-                        })
-                        this.loading = false
-                    },300)
-
-                })
+            parseResponse(response){
+                let {id} = JSON.parse(response)
+                let url = `http://127.0.0.1:3000/preview/${id}`
+                return url
             },
-            edit(id){
-                alert(`正在编辑第${id}个`)
-            },
-            view(id){
-                alert(`正在查看第${id}个`)
+            x(){
+                console.log(this.fileList.length);
             }
         },
         watch: {
-          orderBy(val,oldVal){
-              for(let key in this.orderBy){
-                  if(val[key] !== oldVal[key]){
-                      this.key = key
-                  }
-              }
-          }
+
         },
         created() {
 
