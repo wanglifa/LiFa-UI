18790cebe956ab15ef2fabaf6db7fc899a8c962d
wanglifa <wanglifa1995@gmail.com>
2019-05-20 22:58:19 +0800

    sticky组件初步实现

--- src/demo.vue
+++ src/demo.vue
@@ -1,25 +1,29 @@
 <template>
-    <div>
-        {{error}}
-        <br>
-        <div>只能上传300kb以内的png、jpeg文件</div>
-        <lf-upload accept="image/*" action="http://node-server-19.herokuapp.com/upload" name="file"
-            :file-list.sync="fileList" :parse-response="parseResponse"
-                   @error="error=$event" multiple
-        >
-            <lf-button icon="upload">上传</lf-button>
-        </lf-upload>
+    <div style="justify-content: center;display: flex">
+        <div style="width: 600px;border: 1px solid red">
+            <lf-sticky distance="100">
+                <div style="border: 1px solid red;">
+                    <img src="https://i.loli.net/2019/05/20/5ce2b50b09e2843554.png" alt="">
+                </div>
+            </lf-sticky>
+            <p>段落1</p>
+            <p>段落1</p>
+            <p>段落1</p>
+            <p>段落1</p>
+            <p>段落1</p>
+            <p>段落1</p>
+            <p>段落1</p>
+            <p>段落1</p>
+        </div>
     </div>
 </template>
 
 <script>
-    import LfUpload from './upload.vue'
-    import LfButton from './button/button.vue'
+    import LfSticky from './sticky.vue'
     export default {
         name: "demo",
         components: {
-            LfUpload,
-            LfButton
+            LfSticky
         },
         data() {
             return {
@@ -58,6 +62,8 @@
 </script>
 
 <style scoped lang="scss">
-
+img{
+    width: 100%;
+}
 
 </style>
---
+++ src/sticky.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="lifa-sticky-wrapper" ref="sticky" :style="{height}">
+        <div class="lifa-sticky" :class="classes" :style="{left,width}">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LiFaSticky",
+        data() {
+            return {
+                sticky: false,
+                height: undefined,
+                width: undefined,
+                left: undefined
+            }
+        },
+        created() {
+
+        },
+        mounted() {
+            // 在一开始还没有脱离文档流的时候获取一下父元素的高度，然后将他的
+            // 高度赋值给它，这样当脱离文档流的时候他也会有高度
+            let top = this.top()
+            this.windowScrollHandler = () => {
+                if(window.scrollY > top) {
+                    // 滚动的时候直接获取高度，防止因为网络延迟原因获取高度不准确
+                    let {left,width,height} = this.$refs.sticky.getBoundingClientRect()
+                    this.left = left + 'px'
+                    this.height = height + 'px'
+                    this.width = width + 'px'
+                    this.sticky = true
+                }else {
+                    this.sticky = false
+                }
+            }
+            window.addEventListener('scroll', this.windowScrollHandler)
+        },
+        methods: {
+            top() {
+                let {top} = this.$refs.sticky.getBoundingClientRect()
+                top = top + window.scrollY
+                return top
+            }
+        },
+        computed: {
+            classes() {
+                return {
+                    sticky: this.sticky
+                }
+            }
+        },
+        beforeDestroy() {
+            window.removeEventListener('scroll',this.windowScrollHandler)
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .lifa-sticky{
+        &.sticky {
+            position: fixed;
+            top: 0;
+        }
+    }
+</style>
